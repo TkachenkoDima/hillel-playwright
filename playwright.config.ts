572eaa83b6d43985577b1778d941c55401bb4a0b
Tkachenko Dima <dtkachenko@s-pro.io>
2025-01-20 21:49:44 +0200

    Initial project setup

---
+++ playwright.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from '@playwright/test';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+export default defineConfig({
+  testDir: 'apps/src/tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+  ],
+  use: {
+    headless: true,
+    actionTimeout: 0,
+    trace: 'on',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure', 
+  },
+
+  projects: [
+    {
+      name: 'ui-tests',
+      testDir: 'tests/ui',
+      use: {
+        headless: false,
+        video: 'retain-on-failure',  
+      },
+    },
+    {
+      name: 'api-tests',
+      testDir: 'tests/api',
+      use: {
+        headless: true,
+        video: 'retain-on-failure', 
+      },
+    },
+  ],
+});
