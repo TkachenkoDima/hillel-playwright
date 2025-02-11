8cb870d337b2f431b36a174b722bb8dfca136b8b
Tkachenko Dima <dtkachenko@s-pro.io>
2025-02-11 20:53:08 +0200

    added hw28

--- utils/generators.ts
+++ utils/generators.ts
@@ -1,4 +1,12 @@
-import { faker } from '@faker-js/faker';
+import { faker } from "@faker-js/faker";
+
+const carBrands = [
+    { id: 1, title: "Audi", models: [1, 2, 3, 4, 5] },
+    { id: 2, title: "BMW", models: [6, 7, 8, 9, 10] },
+    { id: 3, title: "Ford", models: [11, 12, 13, 14, 15] },
+    { id: 4, title: "Porsche", models: [16, 17, 18] },
+    { id: 5, title: "Fiat", models: [19, 20, 21, 22, 23] }
+];
 
 export function generateRandomString(length: number): string {
     return faker.string.alpha(length);
@@ -17,4 +25,14 @@ export function generatePassword() {
     password += randomDigit;
 
     return password;
+}
+
+export function generateValidCar() {
+    const selectedBrand = faker.helpers.arrayElement(carBrands);
+    const selectedModel = faker.helpers.arrayElement(selectedBrand.models);
+    return {
+        carBrandId: selectedBrand.id,
+        carModelId: selectedModel,
+        mileage: faker.number.int({ min: 1000, max: 200000 })
+    };
 }
